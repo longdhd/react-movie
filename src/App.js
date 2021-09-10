72ccf126b4917ae506a233445ccd208915a9d6b0
LongDang <long.danghoangd@gmail.com>
2021-09-10 20:49:42 +0700

    update_homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { createBrowserHistory } from "history";
 import {Router, Switch} from 'react-router';
 import { HomeTemplate } from "./templates/HomeTemplate/HomeTemplate";
 import Home from "./pages/Home/Home";
+import News from './pages/News/News';
+import Contact from "./pages/Contact/Contact";
 
 export const history = createBrowserHistory();
 
@@ -11,6 +13,9 @@ function App() {
     <Router history={history}>
       <Switch>
         <HomeTemplate path="/" exact Component={Home}/>
+        <HomeTemplate path="/news" exact Component={News}/>
+        <HomeTemplate path="/contact" exact Component={Contact}/>
+
       </Switch>
     </Router>
   );
