a1f8a251093d6f1bca5bbd566bbafa165b169bf5
LongDang <long.danghoangd@gmail.com>
2023-03-04 12:25:11 +0700

    switch https

--- src/util/settings/config.js
+++ src/util/settings/config.js
@@ -1,4 +1,4 @@
-export const DOMAIN = 'http://movieapi.cyberlearn.vn';
+export const DOMAIN = 'https://movieapi.cyberlearn.vn';
 export const TOKEN = 'accessToken';
 export const GROUPID = `GP05`;
 
